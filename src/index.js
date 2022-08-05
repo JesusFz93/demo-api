5e03c79d8c730c5e183321aa0223f51266f9e3cb
Jesus <jesus.fdz93@gmail.com>
2022-08-05 13:30:08 -0500

    heroku

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-// 1. IMPORTACIONES
+// // 1. IMPORTACIONES
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const favicon = require("serve-favicon");
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// // 2. MIDDLEWARES
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static("public"));
+// app.use(favicon(__dirname + "/favicon.ico"));
+
+// // 3. RUTAS
+// app.get("/", (req, res) => {
+//     return res.json({
+//         message: "Hola mundo"
+//     })
+// });
+// app.use("/api/users", require("./routes/users.routes"));
+
+
+// // 4. SERVIDOR
+// app.listen(PORT, () => {
+//     console.log(`Servidor en linea en el puerto ${PORT}`);
+// });
+
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const favicon = require("serve-favicon");
-const app = express();
-const PORT = process.env.PORT || 4000;
+const Server = require("./server");
 
-// 2. MIDDLEWARES
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-app.use(favicon(__dirname + "/favicon.ico"));
+const server = new Server();
 
-// 3. RUTAS
-app.get("/", (req, res) => {
-    return res.json({
-        message: "Hola mundo"
-    })
-});
-app.use("/api/users", require("./routes/users.routes"));
-
-
-// 4. SERVIDOR
-app.listen(PORT, () => {
-    console.log(`Servidor en linea en el puerto ${PORT}`);
-});
-
+server.execute();
