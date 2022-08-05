518ca5d5408f1555f91e127a1b84d58938c3fd98
Jesus <jesus.fdz93@gmail.com>
2022-08-05 11:50:50 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // 1. IMPORTACIONES
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4001;
 
 // 2. MIDDLEWARES
 app.use(express.json());
+app.use(cors());
 
 // 3. RUTAS
 app.use("/api/users", require("./routes/users.routes"));
