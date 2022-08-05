b2bfd339aa195b89972b383140190bb8df22ef85
Jesus <jesus.fdz93@gmail.com>
2022-08-05 12:48:46 -0500

    herku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const favicon = require("serve-favicon");
 const app = express();
 const PORT = process.env.PORT || 4001;
 
